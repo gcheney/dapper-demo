cd709755da644fe166c066fc15372b2e21cd0df9
Glendon Cheney <glendon.cheney@gmail.com>
2017-07-24 17:37:42 -0500

    Add customer model

---
+++ DapperDemo/DapperDemo/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DapperDemo.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
+}
